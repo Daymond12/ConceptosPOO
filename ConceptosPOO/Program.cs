1cfc7fe0ead859c0d05302b78089f927a4bc3cb4
roger davila <drogermoises@hotmail.com>
2022-05-23 17:14:18 -0600

    Add project files.

---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPOO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("POO, Concepts");
+Console.WriteLine("==============");
+
+//Date date1 = new Date(2022,05,17);
+try
+{
+    Console.WriteLine(new Date(2024, 06, 31));
+    
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
+
+
+
